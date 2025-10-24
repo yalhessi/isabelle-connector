7c8268df1db560c0cf179000494e25cdd786b2f4
Yousef Alhessi <yalhessi@gmail.com>
2025-10-24 21:17:09 +0100

    remove old caching

--- isabelle_connector/decorators.py
+++ isabelle_connector/decorators.py
@@ -7,123 +7,123 @@ import pickle
 
 from loguru import logger
 
-DISABLE_CACHE = False
-# OVERRIDE_CACHE = False
+# DISABLE_CACHE = False
+# # OVERRIDE_CACHE = False
 
-MAX_DEPTH = 6
-if DISABLE_CACHE:
-    print("File cache is disabled.")
-
-
-def recursive_hash(value, depth=0, ignore_params=[]):
-    """Hash primitives recursively with maximum depth."""
-    if depth > MAX_DEPTH:
-        return hashlib.md5("max_depth_reached".encode()).hexdigest()
-
-    if isinstance(value, (int, float, str, bool, bytes)):
-        return hashlib.md5(str(value).encode()).hexdigest()
-    elif isinstance(value, (list, tuple)):
-        return hashlib.md5(
-            "".join(
-                [recursive_hash(item, depth + 1, ignore_params) for item in value]
-            ).encode()
-        ).hexdigest()
-    elif isinstance(value, dict):
-        return hashlib.md5(
-            "".join(
-                [
-                    recursive_hash(key, depth + 1, ignore_params)
-                    + recursive_hash(val, depth + 1, ignore_params)
-                    for key, val in value.items()
-                    if key not in ignore_params
-                ]
-            ).encode()
-        ).hexdigest()
-    elif hasattr(value, "__dict__") and value.__class__.__name__ not in ignore_params:
-        return recursive_hash(value.__dict__, depth + 1, ignore_params)
-    else:
-        return hashlib.md5("unknown".encode()).hexdigest()
+# MAX_DEPTH = 6
+# if DISABLE_CACHE:
+#     print("File cache is disabled.")
 
 
-def hash_code(code):
-    return hashlib.md5(code.encode()).hexdigest()
+# def recursive_hash(value, depth=0, ignore_params=[]):
+#     """Hash primitives recursively with maximum depth."""
+#     if depth > MAX_DEPTH:
+#         return hashlib.md5("max_depth_reached".encode()).hexdigest()
+
+#     if isinstance(value, (int, float, str, bool, bytes)):
+#         return hashlib.md5(str(value).encode()).hexdigest()
+#     elif isinstance(value, (list, tuple)):
+#         return hashlib.md5(
+#             "".join(
+#                 [recursive_hash(item, depth + 1, ignore_params) for item in value]
+#             ).encode()
+#         ).hexdigest()
+#     elif isinstance(value, dict):
+#         return hashlib.md5(
+#             "".join(
+#                 [
+#                     recursive_hash(key, depth + 1, ignore_params)
+#                     + recursive_hash(val, depth + 1, ignore_params)
+#                     for key, val in value.items()
+#                     if key not in ignore_params
+#                 ]
+#             ).encode()
+#         ).hexdigest()
+#     elif hasattr(value, "__dict__") and value.__class__.__name__ not in ignore_params:
+#         return recursive_hash(value.__dict__, depth + 1, ignore_params)
+#     else:
+#         return hashlib.md5("unknown".encode()).hexdigest()
 
 
-def file_cache(
-    ignore_params=[],
-    include_source_code=False,
-    disable=False,
-    verbose=False,
-):
-    """Decorator to cache function output based on its inputs, ignoring specified parameters.
-    Ignore parameters are used to avoid caching on non-deterministic inputs, such as timestamps.
-    We can also ignore parameters that are slow to serialize/constant across runs, such as large objects.
-    """
+# def hash_code(code):
+#     return hashlib.md5(code.encode()).hexdigest()
 
-    def decorator(func):
-        if disable:
-            if verbose:
-                print("Cache is disabled for function: " + func.__name__)
-            return func
-        if verbose:
-            print("Cache is enabled for function: " + func.__name__)
-        func_source_code_hash = (
-            hash_code(inspect.getsource(func)) if include_source_code else ""
-        )
 
-        def wrapper(*args, recache=False, **kwargs):
-            cache_dir = "../cache/file_cache"
-            os.makedirs(cache_dir, exist_ok=True)
+# def file_cache(
+#     ignore_params=[],
+#     include_source_code=False,
+#     disable=False,
+#     verbose=False,
+# ):
+#     """Decorator to cache function output based on its inputs, ignoring specified parameters.
+#     Ignore parameters are used to avoid caching on non-deterministic inputs, such as timestamps.
+#     We can also ignore parameters that are slow to serialize/constant across runs, such as large objects.
+#     """
 
-            # Convert args to a dictionary based on the function's signature
-            args_names = func.__code__.co_varnames[: func.__code__.co_argcount]
-            args_dict = dict(zip(args_names, args))
-            # print(args_dict)
-            # exit()
+#     def decorator(func):
+#         if disable:
+#             if verbose:
+#                 print("Cache is disabled for function: " + func.__name__)
+#             return func
+#         if verbose:
+#             print("Cache is enabled for function: " + func.__name__)
+#         func_source_code_hash = (
+#             hash_code(inspect.getsource(func)) if include_source_code else ""
+#         )
 
-            # Remove ignored params
-            kwargs_clone = kwargs.copy()
-            for param in ignore_params:
-                args_dict.pop(param, None)
-                kwargs_clone.pop(param, None)
+#         def wrapper(*args, recache=False, **kwargs):
+#             cache_dir = "../cache/file_cache"
+#             os.makedirs(cache_dir, exist_ok=True)
 
-            # Create hash based on argument names, argument values, and function source code
-            arg_hash = (
-                recursive_hash(args_dict, ignore_params=ignore_params)
-                + recursive_hash(kwargs_clone, ignore_params=ignore_params)
-                + func_source_code_hash
-            )
-            cache_file = os.path.join(
-                cache_dir, f"{func.__module__}_{func.__name__}_{arg_hash}.pickle"
-            )
+#             # Convert args to a dictionary based on the function's signature
+#             args_names = func.__code__.co_varnames[: func.__code__.co_argcount]
+#             args_dict = dict(zip(args_names, args))
+#             # print(args_dict)
+#             # exit()
 
-            try:
-                # If cache exists, load and return it
-                if os.path.exists(cache_file) and not recache:
-                    if verbose:
-                        print(
-                            "Used cache for function: "
-                            + func.__name__
-                            + " with args: "
-                            + str(args_dict)
-                        )
-                    with open(cache_file, "rb") as f:
-                        return pickle.load(f)
-            except Exception:
-                logger.info("Unpickling failed")
+#             # Remove ignored params
+#             kwargs_clone = kwargs.copy()
+#             for param in ignore_params:
+#                 args_dict.pop(param, None)
+#                 kwargs_clone.pop(param, None)
 
-            # Otherwise, call the function and save its result to the cache
-            result = func(*args, **kwargs)
-            try:
-                with open(cache_file, "wb") as f:
-                    pickle.dump(result, f)
-            except Exception as e:
-                logger.info(f"Pickling failed: {e}")
-            return result
+#             # Create hash based on argument names, argument values, and function source code
+#             arg_hash = (
+#                 recursive_hash(args_dict, ignore_params=ignore_params)
+#                 + recursive_hash(kwargs_clone, ignore_params=ignore_params)
+#                 + func_source_code_hash
+#             )
+#             cache_file = os.path.join(
+#                 cache_dir, f"{func.__module__}_{func.__name__}_{arg_hash}.pickle"
+#             )
 
-        return wrapper
+#             try:
+#                 # If cache exists, load and return it
+#                 if os.path.exists(cache_file) and not recache:
+#                     if verbose:
+#                         print(
+#                             "Used cache for function: "
+#                             + func.__name__
+#                             + " with args: "
+#                             + str(args_dict)
+#                         )
+#                     with open(cache_file, "rb") as f:
+#                         return pickle.load(f)
+#             except Exception:
+#                 logger.info("Unpickling failed")
 
-    return decorator
+#             # Otherwise, call the function and save its result to the cache
+#             result = func(*args, **kwargs)
+#             try:
+#                 with open(cache_file, "wb") as f:
+#                     pickle.dump(result, f)
+#             except Exception as e:
+#                 logger.info(f"Pickling failed: {e}")
+#             return result
+
+#         return wrapper
+
+#     return decorator
 
 
 def timing(f):
