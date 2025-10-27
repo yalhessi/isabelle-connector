3a587d3490a052d8696ea3a04a1b3e343ecea650
Yousef Alhessi <yalhessi@gmail.com>
2025-10-27 09:10:27 +0000

    clean up utils functions

--- isabelle_connector/utils.py
+++ isabelle_connector/utils.py
@@ -1,54 +1,75 @@
-# import os
-# import dotenv
-# import multiprocessing
+import os
+from typing import Optional
+from uuid import uuid4
+import warnings
 
-# dotenv_loaded = False
-# def load_env():
-#     global dotenv_loaded
-#     if not dotenv_loaded:
-#         dotenv.load_dotenv('.env')
-#         dotenv_loaded = True
-
-# def getenv(*args, **kwargs):
-#     load_env()
-#     return os.getenv(*args, **kwargs)
-
-# def get_run_id(model_name, dataset_name, dataset_config):
-#     def extract_name(name):
-#         return name.split("/")[-1] if "/" in name else name
-
-#     dataset_name = extract_name(dataset_name)
-#     model_name = extract_name(model_name)
-#     return f"{dataset_name}-{dataset_config}-{model_name}"
+from isabelle_connector.isabelle_types import Theory
 
 
-# def get_isabelle_dir():
-#     return getenv("ISABELLE_DIR", "/isabelle")
+def _get_or_create_theory_name(theory_name: Optional[str]) -> str:
+    return (
+        "Temp" + str(uuid4()).replace("-", "") if theory_name is None else theory_name
+    )
 
-# def get_output_dir():
-#     return getenv("OUTPUT_DIR", "/output")
 
-# def get_pisa_port():
-#     return int(getenv("PISA_PORT", 8000))
+def get_working_dirs(dataset_dir):
+    return [
+        wd
+        for wd in os.listdir(dataset_dir)
+        if os.path.isdir(os.path.join(dataset_dir, wd))
+    ]
 
-# def current_process_offset():
-#     return multiprocessing.current_process()._identity[0]
 
-# def flatten_dict(d_list: list[dict]) -> dict:
-#     return {k: v for d in d_list for k, v in d.items()}
+def list_theory_files(root_dir):
+    import glob
+
+    theory_files = glob.glob(os.path.join(root_dir, "**/*.thy"), recursive=True)
+    if not theory_files:
+        warnings.warn(f"No theory files found in {root_dir}")
+    return theory_files
+
+
+def temp_theory(**kwargs):
+    if "name" not in kwargs:
+        kwargs["name"] = _get_or_create_theory_name(None)
+    if "is_temp" not in kwargs:
+        kwargs["is_temp"] = True
+    return Theory(**kwargs)
+
+
+def get_theory(theory_file, root_dir):
+    root_dir = str(root_dir)  # TODO: is it better to keep root_dir as Path object?
+    theory_name = theory_file.removesuffix(".thy").removeprefix(root_dir).strip("/")
+    return Theory(
+        name=theory_name,
+        working_directory=root_dir,
+        imports=[],
+        queries=[],
+        is_temp=False,
+    )
+
+
+def merge_thys(theories):
+    theory = temp_theory(
+        name="Merged" + _get_or_create_theory_name(None),
+        working_directory=theories[0].working_directory,
+        imports=[],
+        queries=[],
+        is_temp=True,
+    )
+    imports = set()
+    for thy in theories:
+        for imprt in thy.imports:
+            imports.add(imprt)
+        theory.queries.extend(thy.queries)
+    theory.imports = list(imports)
+    return theory
+
 
 def flatten(l: list) -> list:
     return [item for sublist in l for item in sublist]
 
-# def prepare_notebook():
-#     import isabelle_connector.reload_recursive    
-#     import nest_asyncio
-#     nest_asyncio.apply()
-#     import warnings
-#     warnings.filterwarnings(action="once")
-#     # import multiprocessing
-#     # multiprocessing.set_start_method("spawn")
-    
+
 def path_to_theory_name(path):
     import re
 
