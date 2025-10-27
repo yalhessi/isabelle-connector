80694162bba49ad1e1c83fa9180812f24e94a936
Yousef Alhessi <yalhessi@gmail.com>
2025-10-27 09:09:10 +0000

    rename file

--- isabelle_connector/data_extraction.py
+++ isabelle_connector/data_extraction.py
@@ -1,9 +1,9 @@
 from argparse import Namespace
 import os
+
 from isabelle_connector.config import INTERIM_DATA_DIR
-from isabelle_connector.isabelle_theory import Theory
-from isabelle_connector.isabelle_utils import temp_theory
-from isabelle_connector.utils import path_to_theory_name
+from isabelle_connector.isabelle_types import Theory
+from isabelle_connector.utils import path_to_theory_name, temp_theory
 
 
 def transitions_theory(thy: Theory, configs: Namespace) -> Theory:
@@ -35,6 +35,7 @@ def transitions_theory(thy: Theory, configs: Namespace) -> Theory:
     )
     thy.add_ml_block(query)
     return thy
+
 
 def hol_session(hol_thy):
     # return "HOL"
@@ -71,6 +72,7 @@ def hol_session(hol_thy):
         session = "-".join(["HOL"] + path.split("/"))
 
     return session
+
 
 def template_and_type_extraction_theory(src_thy: Theory, configs: Namespace) -> Theory:
     name = src_thy.name
@@ -120,34 +122,3 @@ def template_and_type_extraction_theory(src_thy: Theory, configs: Namespace) -> 
     )
     thy.add_ml_block(query)
     return thy
-
-
-# for thy in src_thys:
-#     print(hol_session(thy))
-
-# def transitions_theory(thy: Theory, configs) -> Theory:
-#     """
-#     Get theorems from a theory.
-
-#     :param theory_name: name of the theory
-#     :returns: theorems from the theory
-#     """
-#     query = f"""
-#             let
-#                 val filename = "{thy.working_directory}/{thy.name}.thy"
-#                 val stream = TextIO.openIn filename
-#                 val content = TextIO.inputAll stream
-#                 val theory = @{{theory}}
-#                 val transitions = Extract.parse_text theory content
-#                 val results = map (fn (trans, string) => (Toplevel.name_of trans, string)) transitions;
-#             in
-#                 ("{thy.name}", results)
-#             end"""
-
-#     thy = temp_theory(
-#         name=f"Transitions_{thy.name.replace('/', '_').replace('-', '_')}",
-#         imports=configs.imports,
-#         working_directory=configs.root_dir,
-#     )
-#     thy.add_ml_block(query)
-#     return thy
